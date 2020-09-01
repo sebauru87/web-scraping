65e4a6d0b5c2fa416ff000b0714cba239b906aad
Sebastian <Ferreira>
2020-09-01 15:14:54 -0300

    create and add title, href and votes

--- scrape.py
+++ scrape.py
@@ -1,16 +1,32 @@
-#web scraping hacker news using beautifulsoup4 library
+# web scraping hacker news using beautifulsoup4 library
 import requests
 from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
-#print(soup.find_all('span'))
+# print(soup.find_all('span'))
 
 links = soup.select('.storylink')
-votes = soup.select('.score')
-
-print(links[0], votes[0])
+subtext = soup.select('.subtext')
 
 
+# print(links[0], votes[0])
+
+def create_my_news(links, subtext):
+    hacker_news = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            hacker_news.append({'title': title, 'link': href, 'votes': points})
+
+    return hacker_news
 
 
+# print(votes[0].getText())
+
+
+print(create_my_news(links, subtext))
